868024d6be0f6eb033029c346f249c21476671ac
Adam Parks <adamvp0103@gmail.com>
2025-04-25 14:04:11 -0400

    Display balance

--- src/pages/StatisticsPage/StatisticsPage.tsx
+++ src/pages/StatisticsPage/StatisticsPage.tsx
@@ -1,9 +1,22 @@
+import { useAppSelector } from '../../app/hooks';
 import Nav from '../../components/Nav/Nav';
 
 function StatisticsPage() {
+  const currentUser = useAppSelector((state) => state.users.currentUser);
+  const userTransactions = useAppSelector((state) =>
+    state.transactions.transactions.filter((t) => t.username === currentUser)
+  );
+  const balance = userTransactions.reduce(
+    (prev, curr) =>
+      prev + (curr.type === 'income' ? curr.amount : -curr.amount),
+    0
+  );
+
   return (
     <div>
       <h2>Statistics</h2>
+      <p>BALANCE</p>
+      <p>${balance.toFixed(2)}</p>
       <Nav />
     </div>
   );
