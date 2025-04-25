dfb68465154328f657e36383782964639efb2537
Adam Parks <adamvp0103@gmail.com>
2025-04-25 11:36:54 -0400

    Log out

--- src/pages/AccountPage/AccountPage.tsx
+++ src/pages/AccountPage/AccountPage.tsx
@@ -1,9 +1,20 @@
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import Nav from '../../components/Nav/Nav';
+import { logout } from '../../features/users/usersSlice';
 
 function AccountPage() {
+  const currentUser = useAppSelector((state) => state.users.currentUser);
+  const userData = useAppSelector((state) =>
+    state.users.users.find((u) => u.username === currentUser)
+  );
+  const dispatch = useAppDispatch();
+
   return (
     <div>
       <h2>Account</h2>
+      <p>Hello, {userData?.name}</p>
+      <button onClick={() => dispatch(logout())}>Log Out</button>
+      <button>Delete Account</button>
       <Nav />
     </div>
   );
